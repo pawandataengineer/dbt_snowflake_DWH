e8d209b038998e2a8405ae5d3131b35cdc726150
Pawan Chahar <pawan.chahar.ey@gmail.com>
2024-01-14 18:40:58 +0000

    dim materlized as table

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,15 @@
+
+with src_hosts as 
+ (
+    select *
+    from {{ ref('src_hosts') }}
+ )
+ SELECT
+  host_id, 
+ 
+  nvl(host_name, 'Anonymous') as host_name,
+ is_superhost,
+ created_at,
+ updated_at
+FROM
+ src_hosts
---
+++ models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,18 @@
+with src_listings as 
+(
+    select *
+    from {{ ref('src_listings') }}
+)
+SELECT
+  listing_id,
+ listing_name,
+ listing_url,
+ room_type,
+ case when minimum_nights = 0 then 1
+ else minimum_nights end as minimum_nights,
+ host_id,
+ replace(price_str ,'$') :: Number  as price,
+ created_at,
+ updated_at
+FROM
+ src_listings
